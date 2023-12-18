d250d5a3f0c06394a5bfe28aac7868d44234b62d
ike <67127723+ike-gg@users.noreply.github.com>
2023-12-18 12:37:53 +0100

    Initial Commit

---
+++ personalutilities/personalutilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalutilitiesApp.swift
+//  personalutilities
+//
+//  Created by ike on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct personalutilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
